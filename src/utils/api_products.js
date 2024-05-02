6c28cf6910c7f25f840d47abc0f7b3d117505df5
snackers <pvt.stiggs@gmail.com>
2024-05-02 10:32:41 +0800

    _push

--- src/utils/api_products.js
+++ src/utils/api_products.js
@@ -20,6 +20,22 @@ export const getProducts = async (category, sort) => {
   }
 };
 
+// import axios from "axios";
+
+// const url = "http://localhost:5000";
+
+// export const getProducts = async (category) => {
+//   try {
+//     let params = {};
+//     if (category !== "all") params.category = category;
+//     const query = new URLSearchParams(params);
+//     const res = await axios.get(`${url}/products?${query.toString()}`);
+//     return res.data;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
 export const getProduct = async (product_id) => {};
 export const addProduct = async (product_id) => {};
 export const updateProduct = async (product_id) => {};
