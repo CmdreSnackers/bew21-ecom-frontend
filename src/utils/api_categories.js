881d7bfbd97df0cee08ef396481f12a26e36daae
snackers <pvt.stiggs@gmail.com>
2024-05-02 10:20:00 +0800

    _fix

--- src/utils/api_categories.js
+++ src/utils/api_categories.js
@@ -1,12 +1,25 @@
+// import axios from "axios";
+
+// const API_URL = "http://localhost:5000";
+
+// export const getCategories = async () => {
+//   try {
+//     const res = await axios.get(API_URL + "/categories");
+//     return res.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const url = "https://psychic-fiesta-9qr479q7xq3646-5000.app.github.dev";
 
 export const getCategories = async () => {
   try {
-    const res = await axios.get(API_URL + "/categories");
+    const res = await axios.get(`${url}/categories`);
     return res.data;
-  } catch (error) {
-    console.log(error);
+  } catch (e) {
+    console.log(e);
   }
 };
