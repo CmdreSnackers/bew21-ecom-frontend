962ca1693b013e14e4b8ca8e26c0f810acad6083
snackers <pvt.stiggs@gmail.com>
2024-05-02 10:33:51 +0800

    _for_real_this_time

--- src/utils/api_products.js
+++ src/utils/api_products.js
@@ -22,7 +22,7 @@
 
 import axios from "axios";
 
-const url = "https://psychic-fiesta-9qr479q7xq3646-5000.app.github.dev";
+const url = "http://localhost:5000";
 
 export const getProducts = async (category) => {
   try {
